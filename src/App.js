67baade477b2ceebb72b66e98c63c0f865ea9913
neerajkumar790652 <92433406+neerajkumar790652@users.noreply.github.com>
2023-01-16 22:03:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+	const [text, setText] = useState('Search Movie');
+	const [movie, setMovie] = useState([]);
+
+	const changeText = (e) => {
+		// console.log(e);
+		setText(e.target.value);
+	};
+	const getMovie = (e) => {
+		e.preventDefault();
+		axios
+			.get(`http://www.omdbapi.com/?s=${text}&apikey=9af52424`)
+			.then((response) => {
+				console.log(response);
+				setMovie(response.data.Search);
+			});
+	};
+	return (
+		<>
+			<nav className="navbar navbar-dark bg-dark">
+				<div className="container-fluid">
+					<a className="navbar-brand">Movie Search Application</a>
+					<form className="d-flex" role="search" onSubmit={getMovie}>
+						<input
+							className="form-control me-4"
+							type="search"
+							placeholder="Search Movie"
+							aria-label="Search"
+							value={text}
+							onChange={changeText}
+						/>
+						<button className="btn btn-outline-success" type="submit">
+							Search
+						</button>
+					</form>
+				</div>
+			</nav>
+			<div className="container my-2 ">
+				<div className="row">
+					{movie.map((value, index) => {
+						return (
+							<div className="col-3 my-2 py-2">
+								<div className="card" style={{ width: '300px',height:'550px',padding:'30px',margin:'10px' }}>
+									<img src={value.Poster} className="card-img-top" alt="Not found" style={{width:'200px',height:'300px'}}/>
+									<div className="card-body">
+										<h3 className="card-title">{value.Year}</h3>
+										<h4 className="card-text">{value.Title}</h4>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</>
+	);
+};
+export default App;
